daba4f43c01127e6c278864e61d9b71dd757e4b4
darkeynight@gmail.com <kok@gmail.com>
2026-01-06 22:25:34 +0500

    Deleted unnecessary file

--- src/models/tempCodeRunnerFile.java
+++
@@ -1,9 +0,0 @@
-public void sortEventsByTime() {
-        Collections.sort(events);
-        System.out.println("\nEvents Sorted by Time!");
-    }
-
-    public void sortEventsByTitle() {
-        events.sort(Comparator.comparing(Event::getTitle));
-        System.out.println("\nEvents Sorted by Title!");
-    }
